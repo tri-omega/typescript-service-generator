37732cb0305b90b846d97e725c205a27b6db9621
kibork <info@williamfrank.net>
2018-05-08 22:19:14 -0400

    clean

--- src/test/java/org/omega/typescript/processor/CompositPropertyBuilderTest.java
+++ src/test/java/org/omega/typescript/processor/CompositPropertyBuilderTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.omega.typescript.processor.model.Endpoint;
 import org.omega.typescript.processor.model.EndpointMethod;
 import org.omega.typescript.processor.model.PropertyDefinition;
-import org.omega.typescript.processor.test.dto.SimpleDto;
 
 import java.util.List;
 
@@ -46,7 +45,7 @@ public class CompositPropertyBuilderTest {
         assertEquals(2, properties.size());
 
         checkProperty(properties.get(0), "field1", "getField1", "String");
-        checkProperty(properties.get(1), "simpleDto", "getSimpleDto", SimpleDto.class.getSimpleName());
+        checkProperty(properties.get(1), "simpleDto", "getSimpleDto", "SimpleDto");
     }
 
 
