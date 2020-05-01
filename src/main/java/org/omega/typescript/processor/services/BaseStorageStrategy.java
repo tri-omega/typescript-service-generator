7283ddd1799fbd0d18f2d2bba3b6e5c72bc16bd5
kibork <info@williamfrank.net>
2020-05-01 14:33:50 -0400

    Java 11 updates

---
+++ src/main/java/org/omega/typescript/processor/services/BaseStorageStrategy.java
@@ -0,0 +1,45 @@
+package org.omega.typescript.processor.services;
+
+import org.omega.typescript.processor.model.Endpoint;
+import org.omega.typescript.processor.model.TypeDefinition;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by kibork on 5/1/2020.
+ */
+public abstract class BaseStorageStrategy implements StorageStrategy {
+
+    // ---------------- Fields & Constants --------------
+
+    protected final ProcessingContext context;
+
+    protected final FileNamingStrategy fileNamingStrategy;
+
+    // ------------------ Logic      --------------------
+
+    public BaseStorageStrategy(final ProcessingContext context, final FileNamingStrategy fileNamingStrategy) {
+        this.context = context;
+        this.fileNamingStrategy = fileNamingStrategy;
+    }
+
+    @Override
+    public PrintWriter createWriter(final TypeDefinition definition) throws IOException {
+        return createWriter(fileNamingStrategy.getFullTypeFileName(definition));
+    }
+
+    @Override
+    public PrintWriter createWriter(final Endpoint endpoint) throws IOException {
+        return createWriter(fileNamingStrategy.getGetFullFileName(endpoint));
+    }
+
+    @Override
+    public PrintWriter createWriter(final String filename) throws IOException {
+        final File targetFile = getFile(filename);
+        return new PrintWriter(new FileWriter(targetFile, false));
+    }
+
+}
--- src/main/java/org/omega/typescript/processor/services/FileStorageStrategy.java
+++ src/main/java/org/omega/typescript/processor/services/FileStorageStrategy.java
@@ -22,26 +22,16 @@
 
 package org.omega.typescript.processor.services;
 
-import org.omega.typescript.processor.model.Endpoint;
-import org.omega.typescript.processor.model.TypeDefinition;
-
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by kibork on 5/2/2018.
  */
-public class FileStorageStrategy implements StorageStrategy {
+public class FileStorageStrategy extends BaseStorageStrategy {
 
     // ------------------ Constants  --------------------
 
     // ------------------ Fields     --------------------
-
-    private final ProcessingContext context;
-
-    private final FileNamingStrategy fileNamingStrategy;
 
     // ------------------ Properties --------------------
 
@@ -49,19 +39,7 @@ public class FileStorageStrategy implements StorageStrategy {
 
 
     public FileStorageStrategy(final ProcessingContext context, final FileNamingStrategy fileNamingStrategy) {
-        this.context = context;
-        this.fileNamingStrategy = fileNamingStrategy;
-    }
-
-    @Override
-    public PrintWriter createWriter(final TypeDefinition definition) throws IOException {
-        return createWriter(fileNamingStrategy.getFullTypeFileName(definition));
-    }
-
-    @Override
-    public PrintWriter createWriter(final String filename) throws IOException {
-        final File targetFile = getFile(filename);
-        return new PrintWriter(new FileWriter(targetFile, false));
+        super(context, fileNamingStrategy);
     }
 
     @Override
@@ -81,10 +59,5 @@ public class FileStorageStrategy implements StorageStrategy {
         return targetFile;
     }
 
-
-    @Override
-    public PrintWriter createWriter(final Endpoint endpoint) throws IOException {
-        return createWriter(fileNamingStrategy.getGetFullFileName(endpoint));
-    }
 
 }
